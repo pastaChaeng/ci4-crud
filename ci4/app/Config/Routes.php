a359de7e7d62273e335b0e56f6b1b2118c4730a6
Yumeko <arlenejoy.nacion777@gmail.com>
2023-08-28 16:36:21 +0800

    crud - add

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Student's route
+$routes->get('/studs', 'studController::index');
+$routes->get('/studs/create', 'studController::createStud');
---
+++ ci4/app/Views/studs/add.php
@@ -0,0 +1,11 @@
+<?php
+
+$this->extend('layout/main');
+$this->section('body');
+
+
+?>
+
+<h1>Add Student</h1>
+
+<?php $this->endSection(); ?>
