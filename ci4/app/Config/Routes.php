6ec89df665a3584e038db54f88fbd1c7b909c46a
Yumeko <arlenejoy.nacion777@gmail.com>
2023-08-30 18:43:54 +0800

    show

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -10,3 +10,7 @@ $routes->get('/', 'Home::index');
 // Student's route
 $routes->get('/studs', 'StudController::index');
 $routes->get('/studs/create', 'StudController::createStuds');
+$routes->post('/studs/store', 'StudController::storeStuds');
+$routes->get('/studs/edit/(:num)', 'StudController::editStuds/$1');
+$routes->post('/studs/update/(:num)', 'StudController::updateStuds/$1');
+$routes->get('/studs/delete/(:num)', 'StudController::deleteStuds/$1');
